dfd7ae1a6046b2fdf2ddbc91d7b8c74d5f1d4f09
erikabrowning <erikabrowning@me.com>
2020-02-09 17:51:30 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,137 @@
+console.log('hello world');
+
+
+// let cat = "meow";
+// if (cat === "meow" ) {
+//   console.log ( "Nice Kitty.");
+// }
+
+if (10>9){
+  console.log('Kitty will meow');
+}
+
+// let name = "Felix"
+// if ('name === Felix') {
+//   console.log ('Felix The Cat!');
+// }
+
+// let age = 20;
+// let cat = "meow";
+
+// if( cat === "meow" && age > 18 ){
+// 	console.log ("You are a very old cat.");
+// }
+
+
+let name = "cece" 
+let occupation = "web developer"
+
+if ( 'name === cece && occupation === web developer' )
+{
+    console.log ("you are correct");
+}
+
+if ('name = cece ||  favebook = Becoming'){
+    console.log ("you are still correct! ");
+}
+
+if ('name === Bebe || favebook === The Art Of The Deal' )
+
+
+console.log ("You are correct!");
+
+let dog1 = "Samson"
+let dog2 = "Hercules"
+let breed = "Havanese"
+
+if ( 'dog1 === Samson && dog2 === Hercules' )
+{
+    console.log (" They are our family dogs ");
+
+}
+
+if ( 'dog1 = Samson || breed === Havanese' ){
+    
+console.log(" They are circus dogs ");
+}
+
+
+if (  'dog2 = Hercules ||  breed === poodle' ) {
+
+    console.log (" we love dogs ");
+}
+
+let myName = "Erika"
+
+
+if ( myName = true ){
+console.log(" Good evening young lady ");
+} else {
+console.log("Good evening young man");
+}
+
+// let cat = "Meow"
+
+// if( cat === "meow" )
+// {
+// 	console.log("Nice kitty");
+// }
+// else {
+// 	console.log("Are you really a cat?");
+// }
+
+
+let cat = "oink";
+
+if( cat === "meow" ){
+	console.log("Nice kitty");
+}
+else if( cat === "ruff" )  {
+	console.log("You are a dog!");
+}
+else if( cat === "nay nay" )  {
+	console.log("You are a horse!");
+}
+else {
+	console.log("Are you really a cat?");
+}
+
+
+let favshow = "Law and Order";
+switch (favshow) 
+{
+    
+    case "Law and Order":
+        console.log("Always interesting and has many seasons");
+
+        break;
+        
+        case "30 Rock ":
+            console.log("Always a good laugh");
+
+            break;
+
+            case "Bobs Burgers":
+            console.log("The best show")
+         
+            break;
+            
+            default:
+
+            console.log("You do not watch much TV");
+
+            break;
+
+            
+            
+            let cat = "meow";
+
+cat === "meow" ? console.log("Nice kitty.") : console.log("Are you really a cat?")
+
+
+
+
+
+
+
+}
